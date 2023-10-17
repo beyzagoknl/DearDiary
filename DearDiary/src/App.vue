e3e35a987b3d8b02a7ed72a9e0d1e25b707fb918
Beyza GÖK <104020458+beyzagoknl@users.noreply.github.com>
2023-10-17 10:31:42 +0200

    initial settings

---
+++ DearDiary/src/App.vue
@@ -0,0 +1,18 @@
+<template>
+<header>
+<div class="container">
+<img src="@/assets/pinia-logo.svg" alt="pinia" title="Pinia">
+<h1>My Diary</h1>
+</div>
+</header>
+</template>
+
+<script>
+  export default {
+    
+  }
+</script>
+
+<style lang="scss" scoped>
+
+</style>
